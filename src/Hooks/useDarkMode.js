b4a1cbef1bde77bdcab7b9510dea1084d7b4a160
Jorge Jimenez <calenocam24@gmail.com>
2020-08-11 20:48:04 -0400

    different way to add or remove dark-mode class-name

--- src/Hooks/useDarkMode.js
+++ src/Hooks/useDarkMode.js
@@ -4,11 +4,24 @@ import { useLocalStorage } from "./useLocalStorage";
 export const useDarkMode = (toggleValue) => {
   const [darkToggle, SetDarkToggle] = useLocalStorage(toggleValue);
 
-  useEffect(() => {
-    darkToggle
-      ? (document.body.className = "dark-mode")
-      : (document.body.className = null);
-  });
+  // useEffect(() => {
+  //   darkToggle
+  //     ? (document.body.className = "dark-mode")
+  //     : (document.body.className = null);
+  // });
+
+  useEffect(
+    () => {
+      const className = "dark-mode";
+      const element = window.document.body;
+      if (darkToggle) {
+        element.classList.add(className);
+      } else {
+        element.classList.remove(className);
+      }
+    },
+    [darkToggle] // Only re-call effect when value changes
+  );
 
   return [darkToggle, SetDarkToggle];
 };
